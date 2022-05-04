f351825e400cbde686896e840e2634f1e9172ad2
jakkrit <jakkritt26@gmail.com>
2022-05-04 16:43:39 +0700

    fetch data

--- pages/fetch/[name].js
+++ pages/fetch/[name].js
@@ -1,2 +1,27 @@
 
-//https://restcountries.com/v3.1/all
+import React, { useEffect, useState } from 'react';
+const FetchName = ({data}) => {
+
+    const [dataSet , setDataset] = useState();
+
+    useEffect(() => {
+        console.log(data)
+        setDataset(data)
+    }, [data])
+    
+    return (
+        <div>
+            {/* <p>name : {dataSet.name.common}</p> */}
+        </div>
+    );
+    
+}
+
+export async function getServerSideProps(name) {
+    
+    const res = await fetch(`https://restcountries.com/v3.1/name/${name.query.name}`)
+    const data = await res.json()
+
+    return { props: { data } }
+  }
+export default FetchName;
